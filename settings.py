5109a3ad06008b75ddb4b40a4202d0c7faa6a3cb
Ryan Conn <rconn478@gmail.com>
2022-05-09 14:22:38 -0500

    Update settings to be for one GPU and use outcrop dataset

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from torch import Tensor
 
 
 # Data settings
-DATA_ROOT = '/path/to/VOC'
+DATA_ROOT = '/path/to/dataset'
 MEAN = Tensor(np.array([0.485, 0.456, 0.406]))
 STD = Tensor(np.array([0.229, 0.224, 0.225]))
 SCALES = (0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)
@@ -12,7 +12,7 @@ CROP_SIZE = 513
 IGNORE_LABEL = 255
 
 # Model definition
-N_CLASSES = 21
+N_CLASSES = 13
 N_LAYERS = 101
 STRIDE = 8
 BN_MOM = 3e-4
@@ -20,8 +20,8 @@ EM_MOM = 0.9
 STAGE_NUM = 3
 
 # Training settings
-BATCH_SIZE = 16
-ITER_MAX = 30000
+BATCH_SIZE = 1
+ITER_MAX = 100000
 ITER_SAVE = 2000
 
 LR_DECAY = 10
@@ -31,10 +31,10 @@ POLY_POWER = 0.9
 WEIGHT_DECAY = 1e-4
 
 DEVICE = 0
-DEVICES = list(range(0, 4))
+DEVICES = [0]
 
 LOG_DIR = './logdir' 
-MODEL_DIR = './models'
+MODEL_DIR = './models/outcrop'
 NUM_WORKERS = 16
 
 logger = logging.getLogger('train')
