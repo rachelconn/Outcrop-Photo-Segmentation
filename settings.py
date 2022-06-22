8801b4b335415c093a19daef29472009b3de8b3d
Ryan Conn <rconn478@gmail.com>
2022-06-21 20:00:50 -0500

    Update DATA_ROOT for use in colab

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ from torch import Tensor
 # Larger crop size, batch size
 
 # Data settings
-DATA_ROOT = 'e:/outcrop'
+DATA_ROOT = 'dataset'
 MEAN = Tensor(np.array([0.485, 0.456, 0.406]))
 STD = Tensor(np.array([0.229, 0.224, 0.225]))
 SCALES = (0.5, 0.6, 0.75, 1.0, 1.25, 1.5)
