93d06af42dc087ecf91d96e8eaff1e42b7d515c7
Ryan Conn <rconn478@gmail.com>
2022-08-03 18:33:22 -0500

    Plot prediction confidence in eval

--- eval.py
+++ eval.py
@@ -31,7 +31,7 @@ class Session:
         self.net = DataParallel(self.net, device_ids=[settings.DEVICE])
         dataset = ValDataset(split=dt_split)
         self.dataloader = DataLoader(dataset, batch_size=1, shuffle=False, 
-                                     num_workers=2, drop_last=False)
+                                     num_workers=0, drop_last=False)
         self.hist = 0
 
     def load_checkpoints(self, name):
@@ -58,34 +58,44 @@ class Session:
             return np.squeeze(tensor.cpu().numpy(), axis=0)
 
         # Plot image
-        ax1 = plt.subplot(1, 3, 1)
+        ax1 = plt.subplot(1, 4, 1)
         ax1.set_title('Image')
         image_to_display = np.transpose(batch_to_example(image), (1, 2, 0)) / 2.64
         ax1.imshow(image_to_display)
 
-        # Convert labels to RGB
-        color_map = cm.get_cmap('hsv', settings.N_CLASSES)(np.linspace(0, 1, settings.N_CLASSES))
-        color_map = np.vstack((color_map, [0., 0., 0., 1.])) # For 255 labels
+        # Convert labels to image of predicted classes
+        label_color_map = cm.get_cmap('gist_rainbow', settings.N_CLASSES)(np.linspace(0, 1, settings.N_CLASSES))
+        label_color_map = np.vstack((label_color_map, [0., 0., 0., 1.])) # For 255 labels
 
-        def label_to_image(labels):
+        def label_to_image(labels, cmap):
             labels = batch_to_example(labels)
             labels[labels == 255] = settings.N_CLASSES
-            output = color_map[labels.flatten()]
+            output = cmap[labels.flatten()]
             r, c = labels.shape[:2]
             return np.reshape(output, (r, c, 4))
 
-        label_image = label_to_image(label)
-        pred_image = label_to_image(pred)
+        label_image = label_to_image(label, label_color_map)
+        pred_image = label_to_image(pred, label_color_map)
+
+        # Convert labels to image of per-pixel confidence
+        conf_color_map = cm.get_cmap('RdYlGn')
+        # TODO: make sure batch size has been removed
+        conf_image = conf_color_map(torch.squeeze(logit.max(dim=1)[0]).cpu().numpy())
 
         # Plot ground truth labels
-        ax2 = plt.subplot(1, 3, 2)
+        ax2 = plt.subplot(1, 4, 2)
         ax2.set_title('Ground truth')
         ax2.imshow(label_image)
 
         # Plot predicted labels
-        ax3 = plt.subplot(1, 3, 3)
+        ax3 = plt.subplot(1, 4, 3)
         ax3.set_title('Predicted')
         ax3.imshow(pred_image)
+
+        # Plot confidence
+        ax3 = plt.subplot(1, 4, 4)
+        ax3.set_title('Confidence')
+        ax3.imshow(conf_image)
 
         plt.show()
 
